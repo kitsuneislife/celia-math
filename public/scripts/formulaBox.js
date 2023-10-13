b0b72aec0072977239592661e96a3e9bb971ebb8
kitsuneislife <81118374+kitsuneislife@users.noreply.github.com>
2023-10-13 17:58:53 +0000

    + Formulas

--- public/scripts/formulaBox.js
+++ public/scripts/formulaBox.js
@@ -57,3 +57,59 @@ function openTriangulo() {
 
   labsform.innerHTML = "<row><span class='labs-form-label'>Base</span><input class='labs-form-input' id='triangulo-input-base' type='number' oninput='validarNumero(this)'></row><row><span class='labs-form-label'>Altura</span><input class='labs-form-input' id='triangulo-input-altura' type='number' oninput='validarNumero(this)'></row><button class='labs-form-submit' onclick='areaTriangulo()'>Calcular</button>"
 }
+
+function openLosango() {
+  labscontainer.style.display = "flex";
+  labssol.style.display = "none";
+
+  labslabel.innerHTML = "Losango";
+  labsformula.innerHTML = "\\(A = \\frac{D \\cdot d}{2}\\)";
+  MathJax.typeset([labsformula]);
+
+  labsdesc.innerHTML = "<row class='labs-desc-row'><span class='labs-desc-icon'>\\(A\\)</span></span> : Área</span></row><row class='labs-desc-row'><span class='labs-desc-icon'>\\(D\\)</span></span> : Diagonal maior</span></row><row><span class='labs-desc-icon'>\\(d\\)</span></span> : Diagonal menor</span></row>";
+  MathJax.typeset([labsdesc]);
+
+  labsform.innerHTML = "<row><span class='labs-form-label'>Diagonal maior</span><input class='labs-form-input' id='losango-input-maior' type='number' oninput='validarNumero(this)'></row><row><span class='labs-form-label'>Diagonal menor</span><input class='labs-form-input' id='losango-input-menor' type='number' oninput='validarNumero(this)'></row><button class='labs-form-submit' onclick='areaLosango()'>Calcular</button>"
+}
+
+function openTrapezio() {
+  labscontainer.style.display = "flex";
+  labssol.style.display = "none";
+
+  labslabel.innerHTML = "Trapézio";
+  labsformula.innerHTML = "\\(A = \\frac{B + b}{2} \\cdot h\\)";
+  MathJax.typeset([labsformula]);
+
+  labsdesc.innerHTML = "<row class='labs-desc-row'><span class='labs-desc-icon'>\\(A\\)</span></span> : Área</span></row><row class='labs-desc-row'><span class='labs-desc-icon'>\\(B\\)</span></span> : Base maior</span></row><row><span class='labs-desc-icon'>\\(b\\)</span></span> : Base menor</span></row><row><span class='labs-desc-icon'>\\(h\\)</span></span> : Altura</span></row>";
+  MathJax.typeset([labsdesc]);
+
+  labsform.innerHTML = "<row><span class='labs-form-label'>Base maior</span><input class='labs-form-input' id='trapezio-input-maior' type='number' oninput='validarNumero(this)'></row><row><span class='labs-form-label'>Base menor</span><input class='labs-form-input' id='trapezio-input-menor' type='number' oninput='validarNumero(this)'></row><row><span class='labs-form-label'>Altura</span><input class='labs-form-input' id='trapezio-input-altura' type='number' oninput='validarNumero(this)'></row><button class='labs-form-submit' onclick='areaTrapezio()'>Calcular</button>"
+}
+
+function openPoligonoRegular() {
+  labscontainer.style.display = "flex";
+  labssol.style.display = "none";
+
+  labslabel.innerHTML = "Polígono Regular";
+  labsformula.innerHTML = "\\(A = \\frac{p}{2} \\cdot a\\)";
+  MathJax.typeset([labsformula]);
+
+  labsdesc.innerHTML = "<row class='labs-desc-row'><span class='labs-desc-icon'>\\(A\\)</span> : Área</row><row class='labs-desc-row'><span class='labs-desc-icon'>\\(p\\)</span> : Perímetro</row><row><span class='labs-desc-icon'>\\(a\\)</span> : Apótema</row>";
+  MathJax.typeset([labsdesc]);
+
+  labsform.innerHTML = "<row><span class='labs-form-label'>Perímetro</span><input class='labs-form-input' id='poligono-input-perimetro' type='number' oninput='validarNumero(this)'></row><row><span class='labs-form-label'>Apótema</span><input class='labs-form-input' id='poligono-input-apotema' type='number' oninput='validarNumero(this)'></row><button class='labs-form-submit' onclick='areaPoligonoRegular()'>Calcular</button>";
+}
+
+function openCirculo() {
+  labscontainer.style.display = "flex";
+  labssol.style.display = "none";
+
+  labslabel.innerHTML = "Círculo";
+  labsformula.innerHTML = "\\(A = \\pi r^2\\)<br>\\(P = 2 \\pi r\\)";
+  MathJax.typeset([labsformula]);
+
+  labsdesc.innerHTML = "<row class='labs-desc-row'><span class='labs-desc-icon'>\\(A\\)</span> : Área</row><row class='labs-desc-row'><span class='labs-desc-icon'>\\(P\\)</span> : Perímetro</row><row><span class='labs-desc-icon'>\\(r\\)</span> : Raio</row>";
+  MathJax.typeset([labsdesc]);
+
+  labsform.innerHTML = "<row><span class='labs-form-label'>Raio</span><input class='labs-form-input' id='circulo-input-raio' type='number' oninput='validarNumero(this)'></row><button class='labs-form-submit' onclick='areaCirculo()'>Calcular</button>";
+}
--- public/scripts/formulas/areas.js
+++ public/scripts/formulas/areas.js
@@ -51,3 +51,92 @@ function areaTriangulo () {
   MathJax.typeset([labsanswer]);
   return A;
 }
+
+function areaLosango () {
+  var losangoinputmaior = document.getElementById("losango-input-maior");
+  var losangoinputmenor = document.getElementById("losango-input-menor");
+  var labsanswer = document.getElementById("labs-answer");
+  var D = parseFloat(losangoinputmaior.value);
+  var d = parseFloat(losangoinputmenor.value);
+
+  if(!D || isNaN(D)) return;
+  if(!d || isNaN(d)) return;
+  var labssol = document.getElementById("labs-sol");
+  labssol.style.display = "flex";
+
+  var A = (D * d) / 2;
+  console.log(A);
+  labsanswer.innerHTML = `\\(A = \\frac{${D} \\cdot ${d}}{2}\\)<br>\\(A = \\frac{${D * d}}{2}\\)<br>\\(A = ${A}\\)`;
+  MathJax.typeset([labsanswer]);
+  return A;
+}
+
+function areaTrapezio() {
+  var trapezioinputmaior = document.getElementById("trapezio-input-maior");
+  var trapezioinputmenor = document.getElementById("trapezio-input-menor");
+  var trapezioinputaltura = document.getElementById("trapezio-input-altura");
+  var labsanswer = document.getElementById("labs-answer");
+
+  var B = parseFloat(trapezioinputmaior.value);
+  var b = parseFloat(trapezioinputmenor.value);
+  var h = parseFloat(trapezioinputaltura.value);
+
+  if (!B || isNaN(B)) return;
+  if (!b || isNaN(b)) return;
+  if (!h || isNaN(h)) return;
+
+  var labssol = document.getElementById("labs-sol");
+  labssol.style.display = "flex";
+
+  var A = (B + b) / 2 * h;
+
+  labsanswer.innerHTML = `\\(A = \\frac{${B} + ${b}}{2} \\cdot ${h}\\)<br>\\(A = \\frac{${B + b}}{2} \\cdot ${h}\\)<br>\\(A = ${A}\\)`;
+  MathJax.typeset([labsanswer]);
+
+  return A;
+}
+
+function areaPoligonoRegular() {
+  var poligonoInputPerimetro = document.getElementById("poligono-input-perimetro");
+  var poligonoInputApotema = document.getElementById("poligono-input-apotema");
+  var labsAnswer = document.getElementById("labs-answer");
+
+  var p = parseFloat(poligonoInputPerimetro.value);
+  var a = parseFloat(poligonoInputApotema.value);
+
+  if (!p || isNaN(p)) return;
+  if (!a || isNaN(a)) return;
+
+  var labssol = document.getElementById("labs-sol");
+  labssol.style.display = "flex";
+
+  var A = (p / 2) * a;
+
+  labsAnswer.innerHTML = `\\(A = \\frac{${p}}{2} \\cdot ${a}\\)<br>\\(A = ${p / 2} \\cdot ${a}\\)<br>\\(A = ${A}\\)`;
+  MathJax.typeset([labsAnswer]);
+
+  return A;
+}
+
+function areaCirculo() {
+  var circuloInputRaio = document.getElementById("circulo-input-raio");
+  var labsAnswer = document.getElementById("labs-answer");
+
+  var r = parseFloat(circuloInputRaio.value);
+
+  if (!r || isNaN(r)) return;
+
+  var labssol = document.getElementById("labs-sol");
+  labssol.style.display = "flex";
+
+  var A = Math.PI * Math.pow(r, 2);
+  var P = 2 * Math.PI * r;
+
+  A = Math.round(A * 100) / 100;
+  P = Math.round(P * 100) / 100;
+
+  labsAnswer.innerHTML = `\\(A = \\pi \\cdot ${r}^2\\)<br>\\(A = \\pi \\cdot ${r * r}\\)<br>\\(A \\approx ${A}\\)<br><br>\\(P = 2 \\cdot \\pi \\cdot ${r}\\)<br>\\(P = 2 \\cdot \\pi \\cdot ${r}\\)<br>\\(P \\approx ${P}\\)`;
+  MathJax.typeset([labsAnswer]);
+
+  return { area: A, perimetro: P };
+}
