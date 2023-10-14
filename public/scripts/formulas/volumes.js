95210f47946a0c7d5fa9d80a5714b7d11288ab56
kitsuneislife <81118374+kitsuneislife@users.noreply.github.com>
2023-10-14 17:05:03 +0000

    + Formulas

---
+++ public/scripts/formulas/volumes.js
@@ -0,0 +1,37 @@
+function volumeCubo () {
+  var volumecuboinputlado = document.getElementById("volumecubo-input-lado");
+  var labsanswer = document.getElementById("labs-answer");
+  var l = parseFloat(volumecuboinputlado.value);
+
+  if(!l || isNaN(l)) return;
+  var labssol = document.getElementById("labs-sol");
+  labssol.style.display = "flex";
+
+  var V = l * l * l;
+  console.log(V);
+  labsanswer.innerHTML = `\\(V = ${l}^3\\)<br>\\(V = ${l} \\cdot ${l} \\cdot ${l}\\)<br>\\(V = ${V}\\)`;
+  MathJax.typeset([labsanswer]);
+  return V;
+}
+
+function volumeParalelepipedo () {
+  var volumeparalelepipedoinputcomprimento = document.getElementById("volumeparalelepipedo-input-comprimento");
+  var volumeparalelepipedoinputlargura = document.getElementById("volumeparalelepipedo-input-largura");
+  var volumeparalelepipedoinputaltura = document.getElementById("volumeparalelepipedo-input-altura");
+  var labsanswer = document.getElementById("labs-answer");
+  var l = parseFloat(volumeparalelepipedoinputcomprimento.value);
+  var c = parseFloat(volumeparalelepipedoinputlargura.value);
+  var h = parseFloat(volumeparalelepipedoinputaltura.value);
+
+  if(!l || isNaN(l)) return;
+  if(!c || isNaN(c)) return;
+  if(!h || isNaN(h)) return;
+  var labssol = document.getElementById("labs-sol");
+  labssol.style.display = "flex";
+
+  var V = l * c * h;
+  console.log(V);
+  labsanswer.innerHTML = `\\(V = ${l} \\cdot ${c} \\cdot ${h}\\)<br>\\(V = ${V}\\)`;
+  MathJax.typeset([labsanswer]);
+  return V;
+}
---
+++ public/scripts/openVolumes.js
@@ -0,0 +1,29 @@
+function openVolumeCubo() {
+  labscontainer.style.display = "flex";
+  labssol.style.display = "none";
+
+  labslabel.innerHTML = "Cubo";
+  //labsformula.innerHTML = "A = l²";
+  labsformula.innerHTML = "\\(V = l^3\\)";
+  MathJax.typeset([labsformula]);
+
+  labsdesc.innerHTML = "<row class='labs-desc-row'><span class='labs-desc-icon'>\\(V\\)</span></span> : Volume</span></row><row class='labs-desc-row'><span class='labs-desc-icon'>\\(l\\)</span></span> : Lado</span></row>";
+  MathJax.typeset([labsdesc]);
+
+  labsform.innerHTML = "<row><span class='labs-form-label'>Lado</span><input class='labs-form-input' id='volumecubo-input-lado' type='number' oninput='validarNumero(this)'></row><button class='labs-form-submit' onclick='volumeCubo()'>Calcular</button>"
+}
+
+function openVolumeParalelepipedo() {
+  labscontainer.style.display = "flex";
+  labssol.style.display = "none";
+
+  labslabel.innerHTML = "Paralelepípedo";
+  //labsformula.innerHTML = "A = l²";
+  labsformula.innerHTML = "\\(V = c \\cdot l \\cdot h\\)";
+  MathJax.typeset([labsformula]);
+
+  labsdesc.innerHTML = "<row class='labs-desc-row'><span class='labs-desc-icon'>\\(V\\)</span></span> : Volume</span></row><row class='labs-desc-row'><span class='labs-desc-icon'>\\(c\\)</span></span> : Comprimento</span></row><row class='labs-desc-row'><span class='labs-desc-icon'>\\(l\\)</span></span> : Largura</span></row><row class='labs-desc-row'><span class='labs-desc-icon'>\\(h\\)</span></span> : Altura</span></row>";
+  MathJax.typeset([labsdesc]);
+
+  labsform.innerHTML = "<row><span class='labs-form-label'>Comprimento</span><input class='labs-form-input' id='volumeparalelepipedo-input-comprimento' type='number' oninput='validarNumero(this)'></row><row><span class='labs-form-label'>Largura</span><input class='labs-form-input' id='volumeparalelepipedo-input-largura' type='number' oninput='validarNumero(this)'></row><row><span class='labs-form-label'>Altura</span><input class='labs-form-input' id='volumeparalelepipedo-input-altura' type='number' oninput='validarNumero(this)'></row><button class='labs-form-submit' onclick='volumeParalelepipedo()'>Calcular</button>"
+}
